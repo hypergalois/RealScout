b6cf3d429a90fc0e58f683f39d09083aa621187d
hypergalois <jose.delgado2@live.u-tad.com>
2024-12-26 01:30:18 +0100

    feat: implement initial tab structure and sign-in screen

---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>Sign In</Text>
+      <Button title="Sign In" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default SignIn;
