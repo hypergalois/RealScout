edfda18973ede797587f238a3f0c8fddcdecb279
hypergalois <jose.delgado2@live.u-tad.com>
2024-12-27 01:09:22 +0100

    feat: enhance SignIn screen with Google login functionality and improved layout

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,56 @@
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleSignIn = () => {
+    // Implement Google Sign In
+  };
+
   return (
-    <View>
-      <Text>Sign In</Text>
-      <Button title="Sign In" onPress={() => {}} />
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to RealScout
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer to {"\n"}
+            <Text className="text-primary-300">Your Dream Home</Text>
+          </Text>
+
+          <Text className="text-lg font-rubik text-center font-black-200 mt-12">
+            Login with Google
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleSignIn}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <Image
+              source={icons.google}
+              className="w-5 h-5"
+              resizeMode="contain"
+            ></Image>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
